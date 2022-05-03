262d404e4060253cba3a4b17bd8046a137522cdb
unknown <alex.pedrola@e-campus.uab.cat>
2022-05-03 16:11:00 +0200

    Altavoz actualizado

--- Codigo/altavoz.py
+++ Codigo/altavoz.py
@@ -8,16 +8,18 @@ Created on Wed Apr 27 16:36:00 2022
 from gtts import gTTS #pip install gtts
 import os
 from playsound import playsound #HAY QUE HACER pip install playsound==1.2.2, si decargamos la más nueva no funcionará
-
+"""
 resultado = gTTS("Prueba", lang="es")
 
 resultado.save("resultado.mp3")
 playsound("resultado.mp3")
-
+"""
 def generarSonido(numeroDeLetra):
+    os.remove("resultado.mp3")
     #El numero se supone que será de 0 a 27
     ASCII = numeroDeLetra + 65
     resultado = chr(ASCII)
     audio = gTTS(resultado, lang="es")
-    resultado.save("resultado.mp3")
+    audio.save("resultado.mp3")
     playsound("resultado.mp3")
+    
